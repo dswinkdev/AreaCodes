7dccca01226c97bc658922c3d69e9c22ad836819
Derricko Swink <dswinkgfx@gmail.com>
2024-08-07 14:56:30 -0400

    Area Codes

--- src/AreaCodes.java
+++ src/AreaCodes.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class AreaCodes {
 
+    public static void intro(){
+        System.out.println("✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯");
+        System.out.println("::: Area  🇺🇸 Codes :::");
+        System.out.println("✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯");
+        System.out.println("(look up any area code)");
+    }
+
     //main method
     public static void main(String[] args) {
 
@@ -10,9 +17,14 @@ public class AreaCodes {
         String[] cities = {"Atlanta", "Chicago", "New York", "Los Angeles", "Las Vegas", "Miami"};
         int[] areaCodes = {404, 470, 770, 678, 312, 773, 212, 718, 917, 646, 213, 323, 702, 305};
 
+        intro();
+
+        int areaCode = 0;
+
+        while(areaCode != -1)
         // user input
-        System.out.println("Enter a area code: ");
-        int areaCode = input.nextInt();
+        System.out.println("\nArea code: ");
+        areaCode = input.nextInt();
 
     }
 
