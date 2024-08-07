2ccc3a7961b8e2b2ea709b662eea03d22ca788cf
Derricko Swink <dswinkgfx@gmail.com>
2024-08-07 13:57:37 -0400

    Scanner

--- src/AreaCodes.java
+++ src/AreaCodes.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class AreaCodes {
 
     //main method
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
     }
 
