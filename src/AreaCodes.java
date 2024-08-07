fdfc6ea49df03f5a6fe501c57d3482f55bcca4bb
Derricko Swink <dswinkgfx@gmail.com>
2024-08-07 14:18:08 -0400

    Area Codes

--- src/AreaCodes.java
+++ src/AreaCodes.java
@@ -8,7 +8,7 @@ public class AreaCodes {
         Scanner input = new Scanner(System.in);
 
         String[] cities = {"Atlanta", "Chicago", "New York", "Los Angeles", "Las Vegas", "Miami"};
-        int[] areaCodes = {404, 770, 678, 256, 205, 334, 305};
+        int[] areaCodes = {404, 470, 770, 678, 312, 773, 212, 718, 917, 646, 213, 323, 702, 305};
 
         // user input
         System.out.println("Enter a area code: ");
