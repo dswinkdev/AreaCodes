4dcf0aaee15147ec2df65ad494ca533533495fa6
Derricko Swink <dswinkgfx@gmail.com>
2024-08-07 17:17:23 -0400

    Area Codes

--- src/AreaCodes.java
+++ src/AreaCodes.java
@@ -11,16 +11,34 @@ public class AreaCodes {
 
     public static void atlanta(){
         System.out.println("City: Atlanta");
+        System.out.println("Rank: 8th largest city in US");
         System.out.println("Population: 6 million (2022)");
-        System.out.println("Zip Codes: 404, 470, 678, 770");
+        System.out.println("Area Codes: 404, 470, 678, 770");
         System.out.println("Fun fact: Atlanta (ATL) is known as the \"City in the Forest\".");
+    }
+
+    public static void newYork(){
+        System.out.println("City: New York");
+        System.out.println("Rank: 4th largest city in US");
+        System.out.println("Population: 8.336 million (2022)");
+        System.out.println("Zip Codes: 212, 646, 332, 917, 718, 347, 929 ");
+        System.out.println("Fun fact: Chicago (CHI) is known as the \"Big Apple\".");
     }
 
     public static void chicago(){
         System.out.println("City: Chicago");
+        System.out.println("Rank: 3th largest city in US");
         System.out.println("Population: 2.665 million (2022)");
         System.out.println("Zip Codes: 312, 773 ");
         System.out.println("Fun fact: Chicago (CHI) is known as the \"Windy City\".");
+    }
+
+    public static void losAngeles(){
+        System.out.println("City: Los Angeles");
+        System.out.println("Rank: 4th largest city in US");
+        System.out.println("Population: 3.822 million (2022)");
+        System.out.println("Zip Codes: 212, 646, 332, 917, 718, 347, 929 ");
+        System.out.println("Fun fact: Chicago (CHI) is known as the \"City of Angels\".");
     }
 
     //main method
@@ -41,7 +59,8 @@ public class AreaCodes {
         while(areaCode != -1) {
 
             // user input
-            System.out.print("\nArea code: ");
+            System.out.println("\npress -1 to exit");
+            System.out.print("Area code: ");
             areaCode = input.nextInt();
 
             // invalid area code
@@ -57,6 +76,12 @@ public class AreaCodes {
             // Chicago
             if (areaCode == 312 || areaCode == 773){
                 chicago();
+            }
+
+            // New York
+            if (areaCode == 212 || areaCode == 646 || areaCode == 332 || areaCode == 917
+            || areaCode == 718 || areaCode == 347 || areaCode == 929){
+                newYork();
             }
 
         }
