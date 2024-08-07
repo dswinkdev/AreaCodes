2b90ce1c8f151b15752576ec60d3c421be4ab4e5
Derricko Swink <dswinkgfx@gmail.com>
2024-08-07 14:12:01 -0400

    User input

--- src/AreaCodes.java
+++ src/AreaCodes.java
@@ -10,6 +10,10 @@ public class AreaCodes {
         String[] cities = {"Atlanta", "Chicago", "New York", "Los Angeles", "Las Vegas", "Miami"};
         int[] areaCodes = {404, 770, 678, 256, 205, 334, 305};
 
+        // user input
+        System.out.println("Enter a area code: ");
+        int areaCode = input.nextInt();
+
     }
 
 }
